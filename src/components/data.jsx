0765f34eda73c598222e19621967e7bcb0b062e8
itsyogihr13 <95956384+itsyogihr13@users.noreply.github.com>
2022-05-18 21:00:20 +0530

    init

---
+++ src/components/data.jsx
@@ -0,0 +1,30 @@
+export const Data = [
+  {
+    id: "1",
+    img_src:
+      "https://i.pinimg.com/originals/3b/9c/37/3b9c37df4d23edb68bcbc9fe5638d8cb.jpg",
+    Price: "RS 2000",
+    brand: "Addidas",
+  },
+  {
+    id: "2",
+    img_src:
+      "https://i.pinimg.com/originals/3b/9c/37/3b9c37df4d23edb68bcbc9fe5638d8cb.jpg",
+    Price: "RS 3000",
+    brand: "Nike",
+  },
+  {
+    id: "3",
+    img_src:
+      "https://i.pinimg.com/originals/3b/9c/37/3b9c37df4d23edb68bcbc9fe5638d8cb.jpg",
+    Price: "RS 8000",
+    brand: "Addidas",
+  },
+  {
+    id: "4",
+    img_src:
+      "https://i.pinimg.com/originals/3b/9c/37/3b9c37df4d23edb68bcbc9fe5638d8cb.jpg",
+    Price: "RS 1000",
+    brand: "woodland",
+  },
+];
---
+++ src/components/home.jsx
@@ -0,0 +1,85 @@
+import { useState } from "react";
+import { Data } from "./data";
+import "./data";
+
+export const Home = () => {
+  const [data, SetData] = useState(Data);
+  console.log(data);
+
+  function handlesort(e) {
+    if (e.target.value === "low") {
+      // getdata.sort((first, second) => first.username - second.username);
+      data.sort((a, b) => {
+        if (a.Price > b.Price) return 1;
+        else return -1;
+      });
+      SetData([...data]);
+    } else if (e.target.value === "high") {
+      data.sort((a, b) => {
+        if (a.Price > b.Price) return -1;
+        else return 1;
+      });
+      SetData([...data]);
+    }
+  }
+
+  function handleprice(e) {}
+
+  <p>Sort according to name</p>;
+
+  function handlebyname(e) {
+    data.sort((a, b) => {
+      if (a.brand > b.brand) {
+        return -1;
+      }
+    });
+    SetData([...data]);
+  }
+
+  function handlebynameasc(e) {
+    // console.log("here");
+    data.sort((a, b) => {
+      if (a.brand < b.brand) {
+        return -1;
+      }
+    });
+    SetData([...data]);
+  }
+
+  function handleimg() {}
+
+  return (
+    <div>
+      <select name="" id="" onClick={handlesort}>
+        <option value="">---sort</option>
+        <option value="high">high</option>
+        <option value="low">low</option>
+      </select>
+
+      <button onClick={handlebyname}>sort by desc</button>
+
+      <button onClick={handlebynameasc}>sort by asc</button>
+
+      <a
+        href="https://i.pinimg.com/originals/3b/9c/37/3b9c37df4d23edb68bcbc9fe5638d8cb.jpg"
+        target="_blank"
+      >
+        <button>checkOut </button>
+      </a>
+      <button value={"low"} onClick={handlesort}>
+        low
+      </button>
+      <button value={"high"} onClick={handlesort}>
+        high
+      </button>
+
+      {data.map((el) => (
+        <div id="main">
+          <img src={el.img_src} alt="" srcset="" onClick={handleimg} />
+          <p>{el.Price} </p>
+          <p>{el.brand} </p>
+        </div>
+      ))}
+    </div>
+  );
+};
